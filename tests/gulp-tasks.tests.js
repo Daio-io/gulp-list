6205c1a09a0803401d02ffb0452c45adf7e308fe
Dai <dai.williams@bbc.co.uk>
2015-02-05 22:43:00 +0000

    Written first set of test cases

--- tests/gulp-tasks.tests.js
+++ tests/gulp-tasks.tests.js
@@ -1,8 +1,24 @@
-var chai = require('chai');
+var expect = require('chai').expect;
+var gulpTasks = require('../index.js');
+var example = require('./fixtures/example.json');
 
 describe('Gulp-Tasks-Tests', function () {
 
+    it('should except a json file from gulp source', function (){
 
 
+
+    });
+
+    it('should error when trying to src file that is not .json', function () {
+
+
+
+    });
+
+    it('should output list of tasks to the console', function () {
+
+
+    });
 
 });
